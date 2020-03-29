a4019447a626dea82d8e978fb42fe726dfa9c582
hoholee12 <hoholee12@naver.com>
2020-03-29 19:45:37 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,28 +1,46 @@
 #include<stdio.h>
 #include<stdint.h>
 #include<stdlib.h>
-#include<sys/mman.h>
 #define PAGE_SIZE 4096
 
 //code + count = 4kb
-typedef struct {
+struct asmbuf {
 	uint8_t code[PAGE_SIZE - sizeof(uint64_t)];
 	uint64_t count;
-} asmbuf;
+};
 
-asmbuf* asmbuf_create(void){
+#ifdef _WIN32
+#include<windows.h>
+struct asmbuf* asmbuf_create(void){
+	DWORD type = MEM_RESERVE | MEM_COMMIT;
+	return VirtualAlloc(NULL, PAGE_SIZE, type, PAGE_READWRITE);
+}
+
+void asmbuf_finalize(struct asmbuf* buf){
+	DWORD old;
+	VirtualProtect(buf, sizeof(*buf), PAGE_EXECUTE_READ, &old);
+}
+
+void asmbuf_free(struct asmbuf* buf){
+	VirtualFree(buf, 0, MEM_RELEASE);
+}
+
+#else
+#include<sys/mman.h>
+struct asmbuf* asmbuf_create(void){
 	int prot = PROT_READ | PROT_WRITE;
 	int flags = MAP_ANONYMOUS | MAP_PRIVATE;
 	return mmap(NULL, PAGE_SIZE, prot, flags, -1, 0);
 }
 
-void asmbuf_finalize(asmbuf* buf){
+void asmbuf_finalize(struct asmbuf* buf){
 	mprotect(buf, sizeof(*buf), PROT_READ | PROT_EXEC);
 }
 
-void asmbuf_free(asmbuf* buf){
+void asmbuf_free(struct asmbuf* buf){
 	munmap(buf, PAGE_SIZE);
 }
+#endif
 
 int main(int argc, char* argv[]){
 	printf("hello,  world!\n");
