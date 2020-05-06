1dfc5efff4a1f78cffbe0ca240ac61de9737a56e
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2020-05-07 02:17:53 +0900

    x86emitter complete!

--- main.cpp
+++ main.cpp
@@ -1,15 +1,74 @@
 #include <windows.h>
-#include <vector>
-#include <iostream>
-#include <cstring>
-#include<cstdint>
+#include "X86Emitter.h"
+
+class myMain: public X86Emitter{
+public:
+	myMain(){
+		uint8_t fuck = 123;
+		uint8_t hello = 100;
+		uint32_t hell = (uint32_t)&fuck;
+		uint32_t var = (uint32_t)&hello;
+
+		std::vector<uint8_t> code;
+		X86Emitter::loadByteToDwordRegA(&code, &hell);
+		X86Emitter::loadByteToDwordRegB(&code, &var);
+		X86Emitter::add_ebx_to_eax(&code);
+
+		X86Emitter::mov_memoryval_to_ebx(&code, &var);
+		X86Emitter::mov_al_to_memoryaddr(&code, &var);
+		
+		X86Emitter::ret(&code);
+
+		for (int i = 0; i < code.size(); i++){
+			printf("%x ", code.at(i));
+		}
+
+		
+		SYSTEM_INFO system_info;
+		GetSystemInfo(&system_info);
+		auto const page_size = system_info.dwPageSize;
+
+		// prepare the memory in which the machine code will be put (it's not executable yet):
+		void* buffer = VirtualAlloc(nullptr, page_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+		// copy the machine code into that memory:
+		std::memcpy(buffer, code.data(), code.size());
+
+		// mark the memory as executable:
+		DWORD dummy;
+		VirtualProtect(buffer, code.size(), PAGE_EXECUTE_READ, &dummy);
+
+		// interpret the beginning of the (now) executable memory as the entry
+		// point of a function taking no arguments and returning a 4-byte int:
+
+
+		typedef int32_t(*dank)(void);
+		using weed = int32_t(*)(void);
+		dank function_ptr = (weed)buffer;
+		//auto const function_ptr = reinterpret_cast<std::int32_t(*)()>(buffer);
+
+
+		// call the function and store the result in a local std::int32_t object:
+		auto const result = function_ptr();
+
+		// free the executable memory:
+		VirtualFree(buffer, 0, MEM_RELEASE);
+
+		// use your std::int32_t:
+		printf("\n%d %d\n", result, hello);
+		
+	}
+
+};
 
 int main()
 {
-
-	int* myVar = new int();
-	*myVar = 10;
-	uint32_t pointer = (uint32_t)myVar;
+	/*
+	uint8_t var = 0;
+	uint8_t var2 = 0;
+	uint8_t var3 = 0;
+	uint8_t var4 = 0;
+	uint32_t pointer = (uint32_t)&var4;
 	uint8_t b0, b1, b2, b3;
 	b0 = (pointer & 0x000000ff);
 	b1 = (pointer & 0x0000ff00) >> 8;
@@ -23,37 +82,8 @@ int main()
 		b0, b1, b2, b3, 
 		0xc3                    // return what's currently in EAX
 	};
+	*/
 
-	SYSTEM_INFO system_info;
-	GetSystemInfo(&system_info);
-	auto const page_size = system_info.dwPageSize;
-
-	// prepare the memory in which the machine code will be put (it's not executable yet):
-	void* buffer = VirtualAlloc(nullptr, page_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-
-	// copy the machine code into that memory:
-	std::memcpy(buffer, code.data(), code.size());
-
-	// mark the memory as executable:
-	DWORD dummy;
-	VirtualProtect(buffer, code.size(), PAGE_EXECUTE_READ, &dummy);
-
-	// interpret the beginning of the (now) executable memory as the entry
-	// point of a function taking no arguments and returning a 4-byte int:
-
-
-	typedef int32_t(*dank)(void);
-	using weed = int32_t(*)(void);
-	dank function_ptr = (weed)buffer;
-	//auto const function_ptr = reinterpret_cast<std::int32_t(*)()>(buffer);
-
-
-	// call the function and store the result in a local std::int32_t object:
-	auto const result = function_ptr();
-
-	// free the executable memory:
-	VirtualFree(buffer, 0, MEM_RELEASE);
-
-	// use your std::int32_t:
-	std::cout << result << "\n";
+	myMain* my = new myMain();
+	return 0;
 }
