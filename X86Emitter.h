89e757b2d6b3e68904ec22e50c9e8e82ffa85fc2
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2020-05-07 04:47:24 +0900

    allocation optmize

--- X86Emitter.h
+++ X86Emitter.h
@@ -4,7 +4,6 @@
 	super simple and easy to understand x86 emitter
 
 
-	TODO: rewrite it to count index and allocate mem first and then append opcode later
 */
 
 #include<iostream>
@@ -42,13 +41,15 @@ public:
 		memoryBlock->push_back(byte2);
 		memoryBlock->push_back(byte3);
 	}
+
 	//get memoryBlock from outside
-	//memoryBlock must have exact size!
-	void init(std::vector<uint8_t>* inputMemoryBlock){
+	//memoryBlock optimization
+	void init(std::vector<uint8_t>* inputMemoryBlock, int index = 0){
 		memoryBlock = inputMemoryBlock;
+		memoryBlock->reserve(memoryBlock->capacity() + index);
 		byte0 = byte1 = byte2 = byte3 = 0;
 	}
-	
+
 	//memoryval must always be dword!
 	//load to register
 	void loadByteToDwordRegA(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ mov_memoryval_to_al(memoryBlock, dword); movzx_al_to_eax(memoryBlock); }
@@ -60,87 +61,87 @@ public:
 
 
 	//convert byte to dword
-	void movzx_al_to_eax(std::vector<uint8_t>* memoryBlock){ init(memoryBlock); addByte(0x0F); addByte(0xB6); addByte(0xC0); }
-	void movzx_bl_to_ebx(std::vector<uint8_t>* memoryBlock){ init(memoryBlock); addByte(0x0F); addByte(0xB6); addByte(0xDB); }
+	void movzx_al_to_eax(std::vector<uint8_t>* memoryBlock){ init(memoryBlock, 3); addByte(0x0F); addByte(0xB6); addByte(0xC0); }
+	void movzx_bl_to_ebx(std::vector<uint8_t>* memoryBlock){ init(memoryBlock, 3); addByte(0x0F); addByte(0xB6); addByte(0xDB); }
 	//convert word to dword
-	void movzx_ax_to_eax(std::vector<uint8_t>* memoryBlock){ init(memoryBlock); addByte(0x0F); addByte(0xB7); addByte(0xC0); }
-	void movzx_bx_to_ebx(std::vector<uint8_t>* memoryBlock){ init(memoryBlock); addByte(0x0F); addByte(0xB7); addByte(0xDB); }
+	void movzx_ax_to_eax(std::vector<uint8_t>* memoryBlock){ init(memoryBlock, 3); addByte(0x0F); addByte(0xB7); addByte(0xC0); }
+	void movzx_bx_to_ebx(std::vector<uint8_t>* memoryBlock){ init(memoryBlock, 3); addByte(0x0F); addByte(0xB7); addByte(0xDB); }
 	//no need for the opposite
 
 
 	//memoryval must always be dword!
 	//load store byte
-	void mov_memoryval_to_al(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock); addByte(0xA0); addDword(dword); }
-	void mov_al_to_memoryval(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock); addByte(0xA2); addDword(dword); }
-	void mov_memoryval_to_bl(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock); addByte(0x8A); addByte(0x1D); addDword(dword); }
-	void mov_bl_to_memoryval(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock); addByte(0x88); addByte(0x1D); addDword(dword); }
+	void mov_memoryval_to_al(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock, 5); addByte(0xA0); addDword(dword); }
+	void mov_al_to_memoryval(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock, 5); addByte(0xA2); addDword(dword); }
+	void mov_memoryval_to_bl(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock, 6); addByte(0x8A); addByte(0x1D); addDword(dword); }
+	void mov_bl_to_memoryval(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock, 6); addByte(0x88); addByte(0x1D); addDword(dword); }
 	//load store word
-	void mov_memoryval_to_ax(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock); addByte(0x66); addByte(0xA1); addDword(dword); }
-	void mov_ax_to_memoryval(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock); addByte(0x66); addByte(0xA3); addDword(dword); }
-	void mov_memoryval_to_bx(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock); addByte(0x66); addByte(0x8B); addByte(0x1D); addDword(dword); }
-	void mov_bx_to_memoryval(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock); addByte(0x66); addByte(0x89); addByte(0x1D); addDword(dword); }
+	void mov_memoryval_to_ax(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock, 6); addByte(0x66); addByte(0xA1); addDword(dword); }
+	void mov_ax_to_memoryval(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock, 6); addByte(0x66); addByte(0xA3); addDword(dword); }
+	void mov_memoryval_to_bx(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock, 7); addByte(0x66); addByte(0x8B); addByte(0x1D); addDword(dword); }
+	void mov_bx_to_memoryval(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock, 7); addByte(0x66); addByte(0x89); addByte(0x1D); addDword(dword); }
 	//load store dword
-	void mov_memoryval_to_eax(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock); addByte(0xA1); addDword(dword); }
-	void mov_eax_to_memoryval(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock); addByte(0xA3); addDword(dword); }
-	void mov_memoryval_to_ebx(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock); addByte(0x8B); addByte(0x1D); addDword(dword); }
-	void mov_ebx_to_memoryval(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock); addByte(0x89); addByte(0x1D); addDword(dword); }
+	void mov_memoryval_to_eax(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock, 5); addByte(0xA1); addDword(dword); }
+	void mov_eax_to_memoryval(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock, 5); addByte(0xA3); addDword(dword); }
+	void mov_memoryval_to_ebx(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock, 6); addByte(0x8B); addByte(0x1D); addDword(dword); }
+	void mov_ebx_to_memoryval(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock, 6); addByte(0x89); addByte(0x1D); addDword(dword); }
 
 
 	//load immediate byte
-	void mov_imm_to_al(std::vector<uint8_t>* memoryBlock, uint8_t byte){ init(memoryBlock); addByte(0xB0); addByte(byte); }
-	void mov_imm_to_bl(std::vector<uint8_t>* memoryBlock, uint8_t byte){ init(memoryBlock); addByte(0xB3); addByte(byte); }
+	void mov_imm_to_al(std::vector<uint8_t>* memoryBlock, uint8_t byte){ init(memoryBlock, 2); addByte(0xB0); addByte(byte); }
+	void mov_imm_to_bl(std::vector<uint8_t>* memoryBlock, uint8_t byte){ init(memoryBlock, 2); addByte(0xB3); addByte(byte); }
 	//load immediate word
-	void mov_imm_to_ax(std::vector<uint8_t>* memoryBlock, uint16_t* word){ init(memoryBlock); addByte(0x66); addByte(0xB8); addWord(word); }
-	void mov_imm_to_bx(std::vector<uint8_t>* memoryBlock, uint16_t* word){ init(memoryBlock); addByte(0x66); addByte(0xBB); addWord(word); }
+	void mov_imm_to_ax(std::vector<uint8_t>* memoryBlock, uint16_t* word){ init(memoryBlock, 4); addByte(0x66); addByte(0xB8); addWord(word); }
+	void mov_imm_to_bx(std::vector<uint8_t>* memoryBlock, uint16_t* word){ init(memoryBlock, 4); addByte(0x66); addByte(0xBB); addWord(word); }
 	//load immediate dword
-	void mov_imm_to_eax(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock); addByte(0xB8); addDword(dword); }
-	void mov_imm_to_ebx(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock); addByte(0xBB); addDword(dword); }
+	void mov_imm_to_eax(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock, 5); addByte(0xB8); addDword(dword); }
+	void mov_imm_to_ebx(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock, 5); addByte(0xBB); addDword(dword); }
 
 
 	//store to addr byte
-	void mov_al_to_memoryaddr(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock); addByte(0x88); addByte(0x04); addByte(0x25); addDword(dword); }
-	void mov_bl_to_memoryaddr(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock); addByte(0x88); addByte(0x1C); addByte(0x25); addDword(dword); }
+	void mov_al_to_memoryaddr(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock, 7); addByte(0x88); addByte(0x04); addByte(0x25); addDword(dword); }
+	void mov_bl_to_memoryaddr(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock, 7); addByte(0x88); addByte(0x1C); addByte(0x25); addDword(dword); }
 	//store to addr word
-	void mov_ax_to_memoryaddr(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock); addByte(0x66); addByte(0x89); addByte(0x04); addByte(0x25); addDword(dword); }
-	void mov_bx_to_memoryaddr(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock); addByte(0x66); addByte(0x89); addByte(0x1C); addByte(0x25); addDword(dword); }
+	void mov_ax_to_memoryaddr(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock, 8); addByte(0x66); addByte(0x89); addByte(0x04); addByte(0x25); addDword(dword); }
+	void mov_bx_to_memoryaddr(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock, 8); addByte(0x66); addByte(0x89); addByte(0x1C); addByte(0x25); addDword(dword); }
 	//store to addr dword
-	void mov_eax_to_memoryaddr(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock); addByte(0x89); addByte(0x04); addByte(0x25); addDword(dword); }
-	void mov_ebx_to_memoryaddr(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock); addByte(0x89); addByte(0x1C); addByte(0x25); addDword(dword); }
+	void mov_eax_to_memoryaddr(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock, 7); addByte(0x89); addByte(0x04); addByte(0x25); addDword(dword); }
+	void mov_ebx_to_memoryaddr(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock, 7); addByte(0x89); addByte(0x1C); addByte(0x25); addDword(dword); }
 
 
 	//add dword
-	void add_eax_to_ebx(std::vector<uint8_t>* memoryBlock){ init(memoryBlock); addByte(0x01); addByte(0xC3); }
-	void add_ebx_to_eax(std::vector<uint8_t>* memoryBlock){ init(memoryBlock); addByte(0x01); addByte(0xD8); }
-	void add_immediate_to_eax(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock); addByte(0x03); addByte(0x05); addDword(dword); }
-	void add_immediate_to_ebx(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock); addByte(0x03); addByte(0x1D); addDword(dword); }
-	void inc_eax(std::vector<uint8_t>* memoryBlock){ init(memoryBlock); addByte(0x40); }
-	void inc_ebx(std::vector<uint8_t>* memoryBlock){ init(memoryBlock); addByte(0x43); }
+	void add_eax_to_ebx(std::vector<uint8_t>* memoryBlock){ init(memoryBlock, 2); addByte(0x01); addByte(0xC3); }
+	void add_ebx_to_eax(std::vector<uint8_t>* memoryBlock){ init(memoryBlock, 2); addByte(0x01); addByte(0xD8); }
+	void add_immediate_to_eax(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock, 6); addByte(0x03); addByte(0x05); addDword(dword); }
+	void add_immediate_to_ebx(std::vector<uint8_t>* memoryBlock, uint32_t* dword){ init(memoryBlock, 6); addByte(0x03); addByte(0x1D); addDword(dword); }
+	void inc_eax(std::vector<uint8_t>* memoryBlock){ init(memoryBlock, 1); addByte(0x40); }
+	void inc_ebx(std::vector<uint8_t>* memoryBlock){ init(memoryBlock, 1); addByte(0x43); }
 	//sub dword
-	void sub_eax_to_ebx(std::vector<uint8_t>* memoryBlock){ init(memoryBlock); addByte(0x29); addByte(0xC3); }
-	void sub_ebx_to_eax(std::vector<uint8_t>* memoryBlock){ init(memoryBlock); addByte(0x29); addByte(0xD8); }
-	void dec_eax(std::vector<uint8_t>* memoryBlock){ init(memoryBlock); addByte(0x48); }
-	void dec_ebx(std::vector<uint8_t>* memoryBlock){ init(memoryBlock); addByte(0x4B); }
+	void sub_eax_to_ebx(std::vector<uint8_t>* memoryBlock){ init(memoryBlock, 2); addByte(0x29); addByte(0xC3); }
+	void sub_ebx_to_eax(std::vector<uint8_t>* memoryBlock){ init(memoryBlock, 2); addByte(0x29); addByte(0xD8); }
+	void dec_eax(std::vector<uint8_t>* memoryBlock){ init(memoryBlock, 1); addByte(0x48); }
+	void dec_ebx(std::vector<uint8_t>* memoryBlock){ init(memoryBlock, 1); addByte(0x4B); }
 
 
 	//bitwise and dword
-	void and_eax_to_ebx(std::vector<uint8_t>* memoryBlock){ init(memoryBlock); addByte(0x21); addByte(0xC3); }
-	void and_ebx_to_eax(std::vector<uint8_t>* memoryBlock){ init(memoryBlock); addByte(0x21); addByte(0xD8); }
+	void and_eax_to_ebx(std::vector<uint8_t>* memoryBlock){ init(memoryBlock, 2); addByte(0x21); addByte(0xC3); }
+	void and_ebx_to_eax(std::vector<uint8_t>* memoryBlock){ init(memoryBlock, 2); addByte(0x21); addByte(0xD8); }
 	//bitwise or dword
-	void or_eax_to_ebx(std::vector<uint8_t>* memoryBlock){ init(memoryBlock); addByte(0x09); addByte(0xC3); }
-	void or_ebx_to_eax(std::vector<uint8_t>* memoryBlock){ init(memoryBlock); addByte(0x09); addByte(0xD8); }
+	void or_eax_to_ebx(std::vector<uint8_t>* memoryBlock){ init(memoryBlock, 2); addByte(0x09); addByte(0xC3); }
+	void or_ebx_to_eax(std::vector<uint8_t>* memoryBlock){ init(memoryBlock, 2); addByte(0x09); addByte(0xD8); }
 	//bitwose xor dword
-	void xor_eax_to_ebx(std::vector<uint8_t>* memoryBlock){ init(memoryBlock); addByte(0x31); addByte(0xC3); }
-	void xor_ebx_to_eax(std::vector<uint8_t>* memoryBlock){ init(memoryBlock); addByte(0x31); addByte(0xD8); }
+	void xor_eax_to_ebx(std::vector<uint8_t>* memoryBlock){ init(memoryBlock, 2); addByte(0x31); addByte(0xC3); }
+	void xor_ebx_to_eax(std::vector<uint8_t>* memoryBlock){ init(memoryBlock, 2); addByte(0x31); addByte(0xD8); }
 	//bitwise shl by immediate dword
-	void shl_imm_eax(std::vector<uint8_t>* memoryBlock, uint8_t byte){ init(memoryBlock); addByte(0xC1); addByte(0xE0); addByte(byte); }
-	void shl_imm_ebx(std::vector<uint8_t>* memoryBlock, uint8_t byte){ init(memoryBlock); addByte(0xC1); addByte(0xE3); addByte(byte); }
+	void shl_imm_eax(std::vector<uint8_t>* memoryBlock, uint8_t byte){ init(memoryBlock, 3); addByte(0xC1); addByte(0xE0); addByte(byte); }
+	void shl_imm_ebx(std::vector<uint8_t>* memoryBlock, uint8_t byte){ init(memoryBlock, 3); addByte(0xC1); addByte(0xE3); addByte(byte); }
 	//bitwise shr by immediate dword
-	void shr_imm_eax(std::vector<uint8_t>* memoryBlock, uint8_t byte){ init(memoryBlock); addByte(0xC1); addByte(0xE8); addByte(byte); }
-	void shr_imm_ebx(std::vector<uint8_t>* memoryBlock, uint8_t byte){ init(memoryBlock); addByte(0xC1); addByte(0xEB); addByte(byte); }
+	void shr_imm_eax(std::vector<uint8_t>* memoryBlock, uint8_t byte){ init(memoryBlock, 3); addByte(0xC1); addByte(0xE8); addByte(byte); }
+	void shr_imm_ebx(std::vector<uint8_t>* memoryBlock, uint8_t byte){ init(memoryBlock, 3); addByte(0xC1); addByte(0xEB); addByte(byte); }
 
 
 	//return from eax
-	void ret(std::vector<uint8_t>* memoryBlock){ init(memoryBlock); addByte(0xC3); }
+	void ret(std::vector<uint8_t>* memoryBlock){ init(memoryBlock, 1); addByte(0xC3); }
 
 };
 
